d6aba3b97099c8cd1a87d70eb0bcec790e110082
Arko Sakib <arko9858@gmail.com>
2017-12-21 02:04:17 +0600

    Completed S02

--- app/src/main/java/com/example/android/sunshine/activity_forecast.java
+++ app/src/main/java/com/example/android/sunshine/activity_forecast.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -19,23 +21,43 @@ import java.util.zip.Inflater;
 public class activity_forecast extends AppCompatActivity {
 
     TextView mWeatherTextView;
+    TextView mErrorTextView;
+    ProgressBar loadingProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        mWeatherTextView = (TextView)findViewById(R.id.tv_weather_data);
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        mErrorTextView = (TextView) findViewById(R.id.tv_error_msg);
+        loadingProgressBar = (ProgressBar) findViewById(R.id.pb_loading);
 
         loadWeatherData();
     }
 
-    public void loadWeatherData(){
+    public void loadWeatherData() {
+        showWeatherDataView();
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new ConnectingToTheInternet().execute(location);
     }
 
-    public class ConnectingToTheInternet extends AsyncTask<String ,Void,String[]>{
+    public void showWeatherDataView() {
+        mErrorTextView.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+
+    public void showErrorMsg() {
+        mErrorTextView.setVisibility(View.VISIBLE);
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+    }
+
+    public class ConnectingToTheInternet extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            loadingProgressBar.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected String[] doInBackground(String... strings) {
@@ -44,11 +66,11 @@ public class activity_forecast extends AppCompatActivity {
             }
             String location = strings[0];
             URL weatherRequestUrl = NetworkUtils.buildUrl(location);
-            try{
+            try {
                 String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
-                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(activity_forecast.this,jsonWeatherResponse);
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(activity_forecast.this, jsonWeatherResponse);
                 return simpleJsonWeatherData;
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
@@ -58,7 +80,9 @@ public class activity_forecast extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] strings) {
+            loadingProgressBar.setVisibility(View.INVISIBLE);
             if (strings != null) {
+                showWeatherDataView();
                 /*
                  * Iterate through the array and append the Strings to the TextView. The reason why we add
                  * the "\n\n\n" after the String is to give visual separation between each String in the
@@ -67,13 +91,15 @@ public class activity_forecast extends AppCompatActivity {
                 for (String weatherString : strings) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
+            }else{
+                showErrorMsg();
             }
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.forecast,menu   );
+        getMenuInflater().inflate(R.menu.forecast, menu);
         return true;
     }
 
@@ -81,7 +107,7 @@ public class activity_forecast extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int selectedItemId = item.getItemId();
 
-        if(selectedItemId == R.id.action_refresh){
+        if (selectedItemId == R.id.action_refresh) {
             loadWeatherData();
             return true;
         }
