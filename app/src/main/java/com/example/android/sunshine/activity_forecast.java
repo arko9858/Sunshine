9bd2d29c054286ab0a63ccc6bf8e8fe9ec6a34d8
Arko Sakib <arko9858@gmail.com>
2017-12-20 22:01:21 +0600

    Completed lesson S02.02

--- app/src/main/java/com/example/android/sunshine/activity_forecast.java
+++ app/src/main/java/com/example/android/sunshine/activity_forecast.java
@@ -1,39 +1,90 @@
 package com.example.android.sunshine;
 
+import android.graphics.Path;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.zip.Inflater;
 
 public class activity_forecast extends AppCompatActivity {
 
-    TextView tv_weather_data;
+    TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        tv_weather_data = (TextView)findViewById(R.id.tv_weather_data);
+        mWeatherTextView = (TextView)findViewById(R.id.tv_weather_data);
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        loadWeatherData();
+    }
 
-        for(String singleData : dummyWeatherData){
-            tv_weather_data.append(singleData+"\n\n\n");
+    public void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new ConnectingToTheInternet().execute(location);
+    }
+
+    public class ConnectingToTheInternet extends AsyncTask<String ,Void,String[]>{
+
+        @Override
+        protected String[] doInBackground(String... strings) {
+            if (strings.length == 0) {
+                return null;
+            }
+            String location = strings[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+            try{
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(activity_forecast.this,jsonWeatherResponse);
+                return simpleJsonWeatherData;
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+
         }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+                /*
+                 * Iterate through the array and append the Strings to the TextView. The reason why we add
+                 * the "\n\n\n" after the String is to give visual separation between each String in the
+                 * TextView. Later, we'll learn about a better way to display lists of data.
+                 */
+                for (String weatherString : strings) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast,menu   );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedItemId = item.getItemId();
+
+        if(selectedItemId == R.id.action_refresh){
+            loadWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
